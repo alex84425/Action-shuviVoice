197d03bc8ee2c2a2f243ed597943aadbfdd90d07
Foris Tsai <foris.tsai@hp.com>
2022-01-04 18:49:01 +0800

    add cookiecutter

--- src/app/config.py
+++ src/app/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "TemplateName"
+    PROJECT_NAME: str = "{{cookiecutter.action_name}}"
     PREFIX: str = f"/action-{PROJECT_NAME.lower()}"
     VERSION: str = "0.0.1"
     ENVIRONMENT: str = os.getenv("ENVIRONMENT", "dev")
