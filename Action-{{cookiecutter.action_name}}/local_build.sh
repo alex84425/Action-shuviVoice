7fc9b16109e5a8760f9d9ba25a2e2adb42152497
Mason Lin <mason.lin1@hp.com>
2022-01-14 17:29:52 +0800

    can be call in windows

--- Action-{{cookiecutter.action_name}}/local_build.sh
+++ Action-{{cookiecutter.action_name}}/local_build.sh
@@ -1,22 +1,14 @@
-#!/bin/bash
-# verify image by `bash local-build.sh`
-
-project_name='project_name'
-docker_name='docker_name'
-prod_docker='prod_docker'
-container_name='container_name'
-
 echo 'Build Dev Image'
-docker build --pull --target=dev-base --network=host -t ${docker_name}:dev-base .
+docker build --pull --target=dev-base --network=host -t docker_name:dev-base .
 
 echo 'Linter'
-docker build --pull --target=dev-linter --network=host -t ${docker_name}:dev-linter .
+docker build --pull --target=dev-linter --network=host -t docker_name:dev-linter .
 
 echo 'Utest Coverage'
-docker build --pull --target=dev-coverage --network=host -t ${docker_name}:dev-coverage .
-docker create --name=${container_name} ${docker_name}:dev-coverage
-docker cp ${container_name}:/var/test-output/. .
-docker rm -v ${container_name}
+docker build --pull --target=dev-coverage --network=host -t docker_name:dev-coverage .
+docker create --name=container_name docker_name:dev-coverage
+docker cp container_name:/var/test-output/. .
+docker rm -v container_name
 
 echo 'Build Prod Image'
-docker build --network=host -t $prod_docker .
+docker build --network=host -t prod_docker .
