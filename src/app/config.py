cc41db47e29a0115da88ad2387f15d11a91aadf7
Foris Tsai <foris.tsai@hp.com>
2022-05-05 10:20:52 +0800

    restructure

--- src/app/config.py
+++ src/app/config.py
@@ -7,7 +7,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Action-{{cookiecutter.action_name}}"
+    PROJECT_NAME: str = "Action-HelloWorld"
     PREFIX: str = os.environ.get("ROOT_PATH", f"/{PROJECT_NAME.lower()}")
     VERSION: str = "0.0.1"
     ENVIRONMENT: str = os.getenv("ENVIRONMENT", "dev")
