eb83928c62b575ba7bbd1fcfb4aad5662c56f9fe
Foris Tsai <foris.tsai@hp.com>
2022-05-06 15:15:40 +0800

    fix middleware bug

--- src/app/main.py
+++ src/app/main.py
@@ -3,7 +3,6 @@ from app.config import get_settings
 from app.health import router as health
 from fastapi import FastAPI
 from vcosmosapiclient.custom_logging import setup_logging
-from vcosmosapiclient.middlewares import RequestContextLogMiddleware
 
 setup_logging()
 
@@ -13,7 +12,6 @@ app = FastAPI(
     version=get_settings().VERSION,
     root_path=get_settings().PREFIX,
 )
-app.add_middleware(RequestContextLogMiddleware)
 
 app.include_router(health.router, tags=["health"])
 app.include_router(action.router, tags=["action"], prefix="/action")
