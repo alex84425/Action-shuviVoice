acd67706fdf4d3d62d442b89ec6ec28e643949e1
Foris Tsai <foris.tsai@hp.com>
2022-05-06 10:04:32 +0800

    rename project name

--- src/app/config.py
+++ src/app/config.py
@@ -7,7 +7,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Action-HelloWorld"
+    PROJECT_NAME: str = "Action-ExecutorTemplate"
     PREFIX: str = os.environ.get("ROOT_PATH", f"/{PROJECT_NAME.lower()}")
     VERSION: str = "0.0.1"
     ENVIRONMENT: str = os.getenv("ENVIRONMENT", "dev")
--- update_action_name.cmd
+++ update_action_name.cmd
@@ -6,8 +6,8 @@ set /p ActionName=New Action Name:
 
 
 set file=docker-compose.yml
-powershell -command "$action_name = '%ActionName%'.ToLower(); (Get-Content %file%) -Replace 'hellowworld', $action_name | Set-Content %file%"
+powershell -command "$action_name = '%ActionName%'.ToLower(); (Get-Content %file%) -Replace 'executortemplate', $action_name | Set-Content %file%"
 
 set file=src\app\config.py
-powershell -command "$action_name = '%ActionName%'; (Get-Content %file%) -Replace 'HelloWorld', $action_name | Set-Content %file%"
+powershell -command "$action_name = '%ActionName%'; (Get-Content %file%) -Replace 'ExecutorTemplate', $action_name | Set-Content %file%"
 pause
