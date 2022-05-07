62b1e0d1adff44a957c53816344db86a1055b3ea
Mason Lin <mason.lin1@hp.com>
2022-05-07 21:44:04 +0800

    Feat/sample code (#19)
    * sample code for daily run
    
    * pre commit auto format
    
    * fix test
    
    * add one more test

--- src/app/action/router.py
+++ src/app/action/router.py
@@ -10,13 +10,17 @@ https://github.azc.ext.hp.com/BPSVCommonService/Action-Development-Guideline/blo
 """
 
 
+import json
 import logging
+from pathlib import Path
 
+import aiofiles
 from app.action import models
 from app.action.executor import monitor_task_error
 from app.config import Settings, get_fake_settings, get_settings
 from fastapi import APIRouter, Depends, HTTPException, status
 from vcosmosapiclient.api import MonitorFileResponse
+from vcosmosapiclient.api_proxy import execute_on_remote, send_file_to_remote
 from vcosmosapiclient.depends import ApiDepends, FakeDepends
 from vcosmosapiclient.utils import validator
 
@@ -71,15 +75,36 @@ async def post_to_action_dryrun(act: models.MyActionPostModel, api: FakeDepends 
 @router.post("/act")
 @validator.post
 async def post_to_action(act: models.MyActionPostModel, api: ApiDepends = Depends(), config: Settings = Depends(get_settings)):
-    if act.actionData.data.MyTestData is not None:
-        # return direct pass for this template
+    # Sample code for direct pass
+    if act.actionData.data.MyTestData == "PASS":
         raise validator.DirectStatusPass("test passed")
-    else:
-        # never go there for this template
-        body = MonitorFileResponse(
-            task_folder=act.context.workingDirectory,
-            monitor_file="LOGS/ResultDetails.json",
-            result_file="LOGS/ResultDetails.json",
-            status_file="LOGS/status.json",
-        ).dict()
-        return body
+
+    # Sample code for direct fail
+    if act.actionData.data.MyTestData == "FAIL":
+        raise RuntimeError("test failed")
+
+    # Sample code for most use case
+    data_from_atc = act.actionData.data.dict()
+    logging.info(data_from_atc)
+    async with aiofiles.tempfile.TemporaryDirectory() as d:
+        result_path = Path(d, "ResultDetails.json")
+        async with aiofiles.open(result_path, mode="w", encoding="utf-8") as f:
+            await f.write(json.dumps(data_from_atc, indent=4))
+        await send_file_to_remote(
+            act.target, file_path=result_path, remote_path=act.context.workingDirectory / "LOGS", override=True, timeout=60
+        )
+
+    # execute on remote
+    await execute_on_remote(
+        act.target,
+        command=["echo", "PASS", ">", str(act.context.workingDirectory / "LOGS" / "status.json")],
+        working_directory=act.context.workingDirectory,
+    )
+
+    body = MonitorFileResponse(
+        task_folder=act.context.workingDirectory,
+        monitor_file="LOGS/status.json",
+        result_file="LOGS/ResultDetails.json",
+        status_file="LOGS/status.json",
+    ).dict()
+    return body
--- src/tests/utest/test_parallel.py
+++ src/tests/utest/test_parallel.py
@@ -11,7 +11,7 @@ from vcosmosapiclient.depends import ApiDepends, FakeDepends
 
 
 @pytest.mark.asyncio
-async def test_parallel_act(act_raw):
+async def test_parallel_act_direct_pass(act_raw):
     FakeDepends.bios.fake_return["get_bios_on_remote"] = {
         "Manufacturing Programming Mode": "Lock",
         "Serial Number": "0123456789",
@@ -27,6 +27,7 @@ async def test_parallel_act(act_raw):
     for i in range(client_number):
         payload = copy.deepcopy(act_raw)
         payload["task"]["taskId"] = f"{i:08}"
+        payload["actionData"]["data"]["MyTestData"] = "PASS"
         act_list.append(payload)
 
     async with AsyncClient(app=app, base_url="http://test") as ac:
@@ -41,3 +42,32 @@ async def test_parallel_act(act_raw):
         logging.info(response.text)
         assert response.status_code == 200
         assert not response.json().get("errorOccurRequestData", None)
+        assert response.json()["resultStatusGetRequestData"] == "PASS"
+
+
+@pytest.mark.asyncio
+async def test_parallel_act_direct_fail(act_raw):
+    app.dependency_overrides[ApiDepends] = FakeDepends
+    app.dependency_overrides[get_settings] = get_fake_settings
+
+    client_number = 100
+
+    act_list = []
+    for i in range(client_number):
+        payload = copy.deepcopy(act_raw)
+        payload["task"]["taskId"] = f"{i:08}"
+        payload["actionData"]["data"]["MyTestData"] = "FAIL"
+        act_list.append(payload)
+
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        tasks = []
+        for i in range(client_number):
+            tasks.append(ac.post("/action/act", json=act_list[i]))
+
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    for response in list(results):
+        logging.info(response.text)
+        assert response.status_code == 200
+        assert response.json()["errorOccurRequestData"]
+        assert response.json()["resultStatusGetRequestData"] == "FAIL"
