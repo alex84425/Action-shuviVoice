255e40d6678eb92644885ba05cde00c84a310111
Mason Lin <mason.lin1@hp.com>
2022-05-09 13:01:43 +0800

    fix

--- update_action_name.py
+++ update_action_name.py
@@ -6,12 +6,17 @@ action_name = HERE.name.lstrip("Action-")
 print("The new action name is:", action_name)
 
 files = [
-    HERE / "docker-compose.yml",
-    HERE / "docker-compose-debug.yml",
     HERE / "src" / "app" / "config.py",
     HERE / "azure-pipelines.yml",
 ]
 for file in files:
     file.write_text(file.read_text().replace("ExecutorTemplate", action_name))
+
+files = [
+    HERE / "docker-compose.yml",
+    HERE / "docker-compose-debug.yml",
+]
+for file in files:
+    file.write_text(file.read_text().replace("executortemplate", action_name.lower()))
 
 HERE.joinpath("README.md").write_text(f"# Action: {action_name}")
