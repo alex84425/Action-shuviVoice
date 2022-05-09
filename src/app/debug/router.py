30969c558218ae73f195117d7d1882c994294c00
Mason Lin <mason.lin1@hp.com>
2022-05-09 10:31:41 +0800

    remove HTTPBasic (#23)

--- src/app/debug/router.py
+++ src/app/debug/router.py
@@ -9,28 +9,14 @@ from ansi2html import Ansi2HTMLConverter
 from app.config import Settings, get_settings
 from fastapi import APIRouter, Depends, HTTPException, Security, status
 from fastapi.responses import HTMLResponse
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.security.api_key import APIKeyHeader
 
 router = APIRouter()
-security = HTTPBasic()
 api_key_header_auth = APIKeyHeader(
     name="X-API-KEY",
     description="API Token, required for debug",
     auto_error=True,
 )
-
-
-def validate_credentials(credentials: HTTPBasicCredentials = Depends(security), config: Settings = Depends(get_settings)):
-    correct_username = secrets.compare_digest(credentials.username, "admin")
-    correct_password = secrets.compare_digest(credentials.password, config.SOURCE_VERSION[:5])
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect user or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
 
 
 async def verify_api_key(api_key_header: str = Security(api_key_header_auth), config: Settings = Depends(get_settings)):
@@ -64,16 +50,6 @@ def log_file(filename: str, config: Settings = Depends(get_settings)):
         con = Ansi2HTMLConverter()
         data = con.convert(data)
         return data
-    except Exception:
-        return traceback.format_exc()
-
-
-@router.post("/debug_deprecated", response_class=HTMLResponse)
-async def debug_deprecated(cmd: str = "", username: str = Depends(validate_credentials)):
-    try:
-        p = subprocess.run(cmd, capture_output=True, encoding="utf-8", shell=True)  # nosec
-        return f"stdout:\n{p.stdout}\n\nstderr:\n{p.stderr}"
-
     except Exception:
         return traceback.format_exc()
 
