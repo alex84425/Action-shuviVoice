cdc36ee8b431849149d792b43ec9fb125dcc257b
Foris Tsai <foris.tsai@hp.com>
2022-05-10 22:54:04 +0800

    fix docker compose

--- local_build.cmd
+++ local_build.cmd
@@ -1,6 +1,7 @@
+@echo off
 echo 'Linter'
 docker build --pull --target=dev-linter --network=host -t docker_name:dev-linter .
-if not %errorlevel% == 0 goto END
+if not %errorlevel% == 0 goto FAIL
 
 
 echo 'Utest Coverage'
@@ -10,17 +11,22 @@ docker create --name=container_name docker_name:dev-coverage
 docker cp container_name:/app/coverage.xml .
 docker cp container_name:/app/htmlcov .
 docker rm -v container_name
-if not %errorcode% == 0 goto END
+if not %errorcode% == 0 goto FAIL
 
 
 echo 'Security'
 docker build --pull --target=dev-security --network=host -t docker_name:dev-security .
-if not %errorlevel% == 0 goto END
+if not %errorlevel% == 0 goto FAIL
 
 
 echo 'Build Prod Image'
 docker build --network=host -t prod_docker .
+if not %errorlevel% == 0 goto FAIL
+echo Test Pass
+goto END
 
+:FAIL
+echo Test Fail
 
 :END
 pause
