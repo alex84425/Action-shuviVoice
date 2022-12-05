5a1727c1d473eb69a36e339ccef638986f2a4d7f
Mason Lin <mason.lin1@hp.com>
2022-12-05 19:00:15 +0800

    fix name checking

--- scripts/update_action_name.py
+++ scripts/update_action_name.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent.resolve()
@@ -6,7 +7,16 @@ print(f"folder name: {PROJECT_ROOT.name}")
 if "Action-" not in PROJECT_ROOT.name:
     raise ValueError("Wrong repo name, repo should be like 'Action-XXX'")
 
-action_name = PROJECT_ROOT.name.lstrip("Action-")
+
+def get_action_name(root_name):
+    # skip "Action-"
+    pattern = re.compile("^Action-([A-Z][a-zA-Z0-9]{2,20})$")
+    matched = pattern.match(root_name)
+    assert matched, "please check action name format"
+    return matched.group(1)
+
+
+action_name = get_action_name(PROJECT_ROOT.name)
 print("The new action name is:", action_name)
 
 files = [
