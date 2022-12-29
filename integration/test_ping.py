07019133a9f65d2e725b7a4d0984b6b705615956
Mason Lin <mason.lin1@hp.com>
2022-12-29 12:33:25 +0800

    fix assert

--- integration/test_ping.py
+++ integration/test_ping.py
@@ -7,12 +7,12 @@ import pytest
 logging.getLogger().setLevel(logging.DEBUG)
 HOST_IP = os.environ.get("HOST_IP")
 ACTION_NAME = os.environ.get("actionNameLow")  # action-xxx
-assert HOST_IP, "HOST_IP is none, please set it"
-assert ACTION_NAME, "ACTION_NAME is none, please set it"
 
 
 @pytest.mark.parametrize("action_name", [ACTION_NAME])
 def test_testdev_get_action_health_via_container_dns_expect_200ok(action_name):
+    assert action_name, "action_name is none, please set it"
+
     url = f"http://{action_name}:8080/action/health"
     resp = httpx.get(url, proxies={}, timeout=10)
     logging.debug(f"{url=} {resp=} {resp.text=}")
@@ -21,6 +21,9 @@ def test_testdev_get_action_health_via_container_dns_expect_200ok(action_name):
 
 @pytest.mark.parametrize("action_name", [ACTION_NAME])
 def test_testdev_get_action_health_via_nginx_expect_200ok(action_name):
+    assert action_name, "action_name is none, please set it"
+    assert HOST_IP, "HOST_IP is none, please set it"
+
     url = f"https://{HOST_IP}/{action_name}/action/health"
     resp = httpx.get(url, verify=False, proxies={}, timeout=10)
     logging.debug(f"{url=} {resp=} {resp.text=}")
