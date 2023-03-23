8a82ef630944e01463b7f4259161cbd3e3c891bd
Mason Lin <mason.lin1@hp.com>
2023-03-23 10:26:17 +0800

    AB#44371 enable health unit test (#232)
    * enable health unit test
    
    * test: :white_check_mark: fix unit test

--- src/tests/utest/test_ping.py
+++ src/tests/utest/test_ping.py
@@ -2,6 +2,8 @@ import pytest
 from fastapi import HTTPException, status
 from pytest_httpx import HTTPXMock
 
+from app.health import router
+
 
 @pytest.mark.asyncio
 async def test_hello(async_app_client):
@@ -10,33 +12,34 @@ async def test_hello(async_app_client):
 
 
 @pytest.mark.asyncio
-async def test_ping(async_app_client):
+async def test_ping_route(async_app_client):
     response = await async_app_client.get("/ping")
     assert response.status_code == status.HTTP_200_OK
 
 
-@pytest.mark.skip(reason="wrong usage of httpx mock")
 @pytest.mark.asyncio
-async def test_health_normal(async_app_client, httpx_mock: HTTPXMock):
+async def test_health_normal(httpx_mock: HTTPXMock):
     httpx_mock.add_response(method="GET", status_code=404)
-    response = await async_app_client.get("/action/health")
-    assert response.status_code == status.HTTP_200_OK
+    response = await router.health()
+    assert response == {"status": "ok"}
 
 
-@pytest.mark.skip(reason="wrong usage of httpx mock")
 @pytest.mark.asyncio
-async def test_health_uut_exception(async_app_client, httpx_mock: HTTPXMock):
-    httpx_mock.add_exception(HTTPException)
-    with pytest.raises(HTTPException, match=r"fail to access UUT Proxy"):
-        await async_app_client.get("/action/health")
+async def test_health_uut_exception(httpx_mock: HTTPXMock):
+    httpx_mock.add_exception(Exception())
+    with pytest.raises(HTTPException) as exc_info:
+        await router.health()
+    assert exc_info.value.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert "fail to access UUT Proxy" in exc_info.value.detail
 
 
-@pytest.mark.skip(reason="wrong usage of httpx mock")
 @pytest.mark.asyncio
-async def test_health_not_404_response(async_app_client, httpx_mock: HTTPXMock):
+async def test_health_not_404_response(httpx_mock: HTTPXMock):
     httpx_mock.add_response(method="GET", status_code=400)
-    with pytest.raises(HTTPException, match=r"Unexpected response from UUT proxy"):
-        await async_app_client.get("/action/health")
+    with pytest.raises(HTTPException) as exc_info:
+        await router.health()
+    assert exc_info.value.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
+    assert "Unexpected response from UUT proxy" in exc_info.value.detail
 
 
 @pytest.mark.asyncio
