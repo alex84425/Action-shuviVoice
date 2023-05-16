7d5afd352ace9597591bdacc5c99c1973750c336
Zeng <gary.zeng1@hp.com>
2023-05-16 10:27:36 +0800

    try

--- integration/checker.py
+++ integration/checker.py
@@ -1,17 +1,21 @@
 import json
 import os
+import sys
 
 
 def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
     print(f"HP_IDP_SERVICE_ID: {os.environ.get('HP_IDP_SERVICE_ID')}")
     print(f"HP_IDP_SERVICE_SECRET: {os.environ.get('HP_IDP_SERVICE_SECRET')}")
     print(f"GITHUB_STATUS: {os.environ.get('GITHUB_STATUS')}")
     print(f"AZ_PAT_TEST_PLANS: {os.environ.get('AZ_PAT_TEST_PLANS')}")
 
-    with open("inputs.json", "r") as f:
-        data = f.read()
-        print(data)
-        valid_json_str = data.replace("'", '"')
-        print(valid_json_str)
-        json_data = json.loads(valid_json_str)
-        print(json_data)
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        content = f.read()
+    print(content)
+    valid_json_data = content.replace("'", '"')
+    print(valid_json_data)
+    
