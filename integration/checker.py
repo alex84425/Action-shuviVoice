8b9ff4134f5ee90a3c38294d99bcc6092353c6a0
Zeng <gary.zeng1@hp.com>
2023-05-16 10:45:40 +0800

    try

--- integration/checker.py
+++ integration/checker.py
@@ -6,12 +6,15 @@ def main():
     if len(sys.argv) < 2:
         sys.exit(1)
 
-    print(f"HP_IDP_SERVICE_ID: {os.environ.get('HP_IDP_SERVICE_ID')}")
-    print(f"HP_IDP_SERVICE_SECRET: {os.environ.get('HP_IDP_SERVICE_SECRET')}")
-    print(f"GITHUB_STATUS: {os.environ.get('GITHUB_STATUS')}")
+    # print(f"HP_IDP_SERVICE_ID: {os.environ.get('HP_IDP_SERVICE_ID')}")
+    # print(f"HP_IDP_SERVICE_SECRET: {os.environ.get('HP_IDP_SERVICE_SECRET')}")
+    # print(f"GITHUB_STATUS: {os.environ.get('GITHUB_STATUS')}")
+    # print(f"AZ_PAT_TEST_PLANS: {os.environ.get('AZ_PAT_TEST_PLANS')}")
 
+    print("################################################")
     for i in sys.argv:
         print(i)
+    print("################################################")
 
     filename = sys.argv[1]
     print(filename)
