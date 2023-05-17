2403fd1a6d2cc0634f05421ddf9d0eb2e503d173
Zeng <gary.zeng1@hp.com>
2023-05-17 12:36:45 +0800

    try

--- integration/checker.py
+++ integration/checker.py
@@ -5,7 +5,6 @@ import json
 import os
 
 import httpx
-from vcosmosapiclient.integration.atc_api_helper import get_hp_access_token
 
 
 async def main():
@@ -14,13 +13,12 @@ async def main():
 
     # source_version = os.environ["SourceVersion"]
     # repository_name = os.environ["RepositoryName"]
-    hp_idp_service_id = os.environ.get("HP_IDP_SERVICE_ID")
-    hp_idp_service_secret = os.environ.get("HP_IDP_SERVICE_SECRET")
+    vcosmos_token = os.environ.get("VCOSMOS_TOKEN")
     github_status = os.environ.get("GITHUB_STATUS")
     vcosmos_access_host = os.environ.get("VCOSMOS_ACCESS_HOST")
     hp_web_proxy = os.environ.get("HP_WEB_PROXY")
 
-    print(hp_idp_service_id, hp_idp_service_secret, github_status)
+    print(vcosmos_token, github_status, vcosmos_access_host, hp_web_proxy)
 
     # get payload
     parser = argparse.ArgumentParser(description="Feature Test Checker")
@@ -31,14 +29,12 @@ async def main():
     workflow_payload = json.loads(valid_json_data)
 
     job_id = workflow_payload["task_jobid"]
-    hp_access_token = await get_hp_access_token(hp_idp_service_id, hp_idp_service_secret)
     get_job_url = f"https://{vcosmos_access_host}/api/v2/jobs/{job_id}"
-    print(f"{hp_access_token}, {get_job_url}, {hp_web_proxy}")
 
     headers = {
         "accept": "application/json",
         "content-type": "application/json;charset=utf-8",
-        "Authorization": hp_access_token["access_token"],
+        "Authorization": vcosmos_token,
     }
     response = httpx.get(get_job_url, headers=headers, proxies=hp_web_proxy)
     print(response.status_code)
