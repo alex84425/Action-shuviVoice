73150a850c85f65c862ed8f1ed75514535b915af
Mason Lin <mason.lin1@hp.com>
2023-05-18 18:35:50 +0800

    show logging

--- integration/checker.py
+++ integration/checker.py
@@ -5,7 +5,6 @@ import json
 import logging
 import os
 
-import httpx
 from feature_test.helper import BVT_TEST_CASES
 from vcosmosapiclient.integration.atc_api_helper import ATC
 from vcosmosapiclient.integration.feature_test_models import FeatureTestCase, ResultStatus, Task, TaskMode
@@ -13,44 +12,42 @@ from vcosmosapiclient.integration.github_helper import GitHubHelper, State
 
 JOB_MAGIC_INDEX = 0
 TASKS_MAGIC_INDEX = 0
+logging.getLogger().setLevel(logging.DEBUG)
 
 
-class VCOSMOS_API:
-    def __init__(self):
-        self.base_url = os.environ["VCOSMOS_ACCESS_HOST"]
-        self.headers = {
-            "accept": "application/json",
-            "content-type": "application/json;charset=utf-8",
-            "Authorization": os.environ["VCOSMOS_TOKEN"],
-        }
-        self.proxies = {
-            "http://": "https://" + os.environ["HP_WEB_PROXY"],
-            "https://": "https://" + os.environ["HP_WEB_PROXY"],
-        }
+# class VCOSMOS_API:
+#     def __init__(self):
+#         self.base_url = os.environ["VCOSMOS_ACCESS_HOST"]
+#         self.headers = {
+#             "accept": "application/json",
+#             "content-type": "application/json;charset=utf-8",
+#             "Authorization": os.environ["VCOSMOS_TOKEN"],
+#         }
+#         self.proxies = {
+#             "http://": "https://" + os.environ["HP_WEB_PROXY"],
+#             "https://": "https://" + os.environ["HP_WEB_PROXY"],
+#         }
 
-    async def get_job(self, job_id: str):
-        async with httpx.AsyncClient(headers=self.headers, proxies=self.proxies, verify=False, timeout=60) as client:
-            get_job_url = f"https://{self.base_url}/api/v2/jobs/" + job_id
-            job_res = await client.get(get_job_url)
+#     async def get_job(self, job_id: str):
+#         async with httpx.AsyncClient(headers=self.headers, proxies=self.proxies, verify=False, timeout=60) as client:
+#             get_job_url = f"https://{self.base_url}/api/v2/jobs/" + job_id
+#             job_res = await client.get(get_job_url)
 
-        if job_res.status_code != 200:
-            raise Exception("job_res.status_code is not 200")
+#         if job_res.status_code != 200:
+#             raise Exception("job_res.status_code is not 200")
 
-        return job_res.json()
+#         return job_res.json()
 
-    async def get_task(self, task_id: str):
-        async with httpx.AsyncClient(headers=self.headers, proxies=self.proxies, verify=False, timeout=60) as client:
-            get_task_url = f"https://{self.base_url}/api/v2/tasks/{task_id}"
-            task_res = await client.get(get_task_url)
+#     async def get_task(self, task_id: str):
+#         async with httpx.AsyncClient(headers=self.headers, proxies=self.proxies, verify=False, timeout=60) as client:
+#             get_task_url = f"https://{self.base_url}/api/v2/tasks/{task_id}"
+#             task_res = await client.get(get_task_url)
 
-        return task_res.json()
+#         return task_res.json()
 
 
-async def testcase_result_checker_and_update_status(task_res: dict, test_case: FeatureTestCase, github: GitHubHelper):
-    print(f"{task_res=}")
-
+async def testcase_result_checker_and_update_status(test_case: FeatureTestCase, github: GitHubHelper):
     try:
-        test_case.task = Task(task_res, mode=TaskMode.SUBSCRIPTION)
         await test_case.checker(test_case.task)
     except Exception as exc:
         msg = f"{exc.__class__.__name__!s:.40}: {exc!s:.100}"
@@ -146,6 +143,7 @@ async def main():
     try:
         task = await atc_helper.monitor_task(job_id, timeout=60)
     except Exception as exc:
+        logging.debug(f"monitor_task failed, (reason: {exc})")
         await github_helper.update_status(
             state=State.FAILURE,
             target_url=target_url,
@@ -154,8 +152,10 @@ async def main():
         )
     else:
         for test_case in test_cases:
+            logging.debug(f"{test_case.name=} {test_name=}")
             if test_case.name == test_name:
-                await testcase_result_checker_and_update_status(task, test_case, github_helper)
+                test_case.task = Task(task, mode=TaskMode.POLLING)
+                await testcase_result_checker_and_update_status(test_case, github_helper)
 
 
 if __name__ == "__main__":
