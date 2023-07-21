130851b127edc5420343c577d922504436ac18b6
Michael Lee <michael.lee3@hp.com>
2023-07-21 19:15:17 +0800

    AB#50437 refined feature test (#347)
    * refined feature test
    
    * add old format
    
    * refined
    
    * refined

--- integration/feature_test/helper.py
+++ integration/feature_test/helper.py
@@ -5,14 +5,39 @@ from vcosmosapiclient.integration.atc_api_helper import ATC_SINGLETON
 from vcosmosapiclient.integration.feature_test_models import FeatureTestCase, ResultNotMatchError, StatusNotMatchError, Task
 
 
-async def hello_world_example_should_pass_checker(task: Task):
-    logging.debug("hello_world_example_should_pass_checker")
+async def hello_world_single_line_example_should_pass_checker(task: Task):
+    logging.debug("hello_world_single_line_example_should_pass")
 
     if await task.get_status() != "PASS":
         raise StatusNotMatchError
     logging.debug("actual_status as expected")
 
-    expected_result = {"text_bar": "Hello", "text_box": "World"}
+    expected_result = {"operation": "Single Line", "text_bar": "Hello World"}
+    actual_result = await task.get_result()
+    if expected_result != actual_result:
+        raise ResultNotMatchError
+    logging.debug("actual_result as expected even we test very details")
+
+    expected_content = "Hello"
+    async with task.get_stored_files(atc=ATC_SINGLETON) as temp_log_path:
+        for file in temp_log_path.glob("**/*hello.txt"):
+            content = file.read_text().strip()
+            if content != expected_content:
+                raise ResultNotMatchError("The file content is not as expected")
+            break
+        else:
+            raise ResultNotMatchError("Can't find the file we want to check")
+    logging.debug("stored files is as expected!")
+
+
+async def hello_world_multi_line_example_should_pass_checker(task: Task):
+    logging.debug("hello_world_multi_line_example_should_pass")
+
+    if await task.get_status() != "PASS":
+        raise StatusNotMatchError
+    logging.debug("actual_status as expected")
+
+    expected_result = {"operation": "Multi Line", "text_box": "Hello \nWorld"}
     actual_result = await task.get_result()
     if expected_result != actual_result:
         raise ResultNotMatchError
@@ -33,13 +58,21 @@ async def hello_world_example_should_pass_checker(task: Task):
 HERE = Path(__file__).parent
 BVT_TEST_CASES = [
     FeatureTestCase(
-        payload_path=HERE / "hello_world_example_should_pass.json",
-        checker=hello_world_example_should_pass_checker,
-    )
+        payload_path=HERE / "hello_world_single_line_example_should_pass.json",
+        checker=hello_world_single_line_example_should_pass_checker,
+    ),
+    FeatureTestCase(
+        payload_path=HERE / "hello_world_multi_line_example_should_pass.json",
+        checker=hello_world_multi_line_example_should_pass_checker,
+    ),
 ]
 INTEGRATION_TEST_CASES = [
     FeatureTestCase(
-        payload_path=HERE / "hello_world_example_should_pass.json",
-        checker=hello_world_example_should_pass_checker,
-    )
+        payload_path=HERE / "hello_world_single_line_example_should_pass.json",
+        checker=hello_world_single_line_example_should_pass_checker,
+    ),
+    FeatureTestCase(
+        payload_path=HERE / "hello_world_multi_line_example_should_pass.json",
+        checker=hello_world_multi_line_example_should_pass_checker,
+    ),
 ]
