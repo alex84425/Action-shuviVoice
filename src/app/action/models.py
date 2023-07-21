c4ea5ed81c2dd66e12c38bf69b8e9b71afaf2b55
Mason Lin <mason.lin1@hp.com>
2023-07-21 17:39:24 +0800

    AB#50436 add operation example (#345)
    * add operation example
    
    * add operation enum
    
    * add get_model_by_operation
    
    * hist
    
    * use literal
    
    * remove my data model
    
    * fix
    
    * rebuild
    
    * use validator

--- src/app/action/models.py
+++ src/app/action/models.py
@@ -1,17 +1,45 @@
-from pydantic import Field
+from enum import Enum
+from typing import Union
+
+from pydantic import Field, validator
 from vcosmosapiclient.models import BaseActionModel, BaseModel
 
 
-class MyDataModel(BaseModel):
+class OperationEnum(str, Enum):
+    SINGLE_LINE = "Single Line"
+    MULTI_LINE = "Multi Line"
+
+
+class OperationModel(BaseModel):
+    operation: OperationEnum = OperationEnum.SINGLE_LINE
+
+
+class SingleLineModel(OperationModel):
     text_bar: str = Field("", alias="Text Bar")
+
+
+class MultiLineModel(OperationModel):
     text_box: str = Field("numbers 0-9 is not allow in this text box", maxLength=30000, alias="Text Box")
 
+
+class MyDataModel(OperationModel):
     class Config:
-        allow_population_by_field_name = True
+        extra = "allow"
 
 
 class MyActionDataModel(BaseModel):
-    data: MyDataModel
+    data: Union[MyDataModel, SingleLineModel, MultiLineModel]
+
+    @validator("data")
+    def get_model_by_operation(cls, value):  # pylint: disable=E0213
+        value_dict = value.dict(by_alias=False)
+        if value_dict["operation"] == OperationEnum.SINGLE_LINE:
+            return SingleLineModel(**value_dict)
+
+        if value_dict["operation"] == OperationEnum.MULTI_LINE:
+            return MultiLineModel(**value_dict)
+
+        raise ValueError("Operation is not supported")
 
 
 class MyActionPostModel(BaseActionModel):
